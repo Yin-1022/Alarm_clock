4157b2a32ae828eddd58472eece38c1ee46e71e4
Yin-1022 <kv.huang931022@gmail.com>
2025-10-18 15:14:02 +0800

    initProject

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AlarmClockApp());
+}
+class AlarmClockApp extends StatelessWidget {
+  const AlarmClockApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
